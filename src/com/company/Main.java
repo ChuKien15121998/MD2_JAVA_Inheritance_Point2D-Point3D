8a5fec4976cccb029bf782970cccd23f86b158b6
ChuKien15121998 <chuvankien151298@gmail.com>
2022-03-27 10:27:08 +0700

    Done

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Point 2D
+        Point2D point2d = new Point2D(15, 12);
+        System.out.println(point2d);
+
+        // Point 3D
+        Point3D point3d = new Point3D(15, 12, 98);
+        System.out.println(point3d);
+    }
+}
